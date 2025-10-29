f7186b36c3283705e5127ed5c00e37b80cf87355
Michael <michael.elliston95@gmail.com>
2025-10-29 14:22:25 -0500

    modified the getDealership() method in DealershipFileManager class to attempt using a stream reader

--- src/main/java/DealershipFileManager.java
+++ src/main/java/DealershipFileManager.java
@@ -1,16 +1,14 @@
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 
 public class DealershipFileManager {
 
     public Dealership getDealership() {
-        try (BufferedReader bufferedReader = new BufferedReader(new FileReader("src/main/resources/WorkshopFiles/inventory.csv"))) {
+        try (InputStream inputStream = getClass().getResourceAsStream("/inventory.csv")) {
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
 
-            String[] parts = bufferedReader.readLine().split("\\|");
+             String[] parts = bufferedReader.readLine().split("\\|");
 
-            return new Dealership(parts[0], parts[1], parts[2]);
+             return new Dealership(parts[0], parts[1], parts[2]);
 
         } catch (FileNotFoundException e) {
             System.err.println("File not found!: " + e);
